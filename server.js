ef8e49e3803d79527c607e94c89ab7e748bf00cf
Jesus <jesus.reyponce@gmail.com>
2018-08-29 01:13:14 -0500

    First commit - GraphQL server for vpproject

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import expressGraphQL from "express-graphql";
+import schema from "./schema";
+
+const APP_PORT = 4000;
+
+const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema: schema,
+    pretty: true,
+    graphiql: true
+  })
+);
+
+app.listen(APP_PORT, () => {
+  console.log("Server running on port: " + APP_PORT);
+});
