56b4a1ac14a1d5004a9da1d5ab3ce89d61fecbf0
Jesus <jesus.reyponce@gmail.com>
2018-09-20 02:16:02 -0500

    added two more branches for testing purposes

--- conn/db.js
+++ conn/db.js
@@ -63,6 +63,18 @@ conn.sync({ force: true }).then(() => {
     latitude: -12.09752,
     longitude: -77.010324
   });
+  Branch.create({
+    branchName: "Colegio Alpamayo",
+    address: "Ca. Bucaramanga 145",
+    latitude: -12.061806,
+    longitude: -76.937528
+  });
+  Branch.create({
+    branchName: "Shaniko Common",
+    address: "Warm Springs Blvd",
+    latitude: 37.488448,
+    longitude: -121.927627
+  });
   ServiceShift.create({
     date: "18/09",
     begin: "11:00 am",
@@ -75,6 +87,20 @@ conn.sync({ force: true }).then(() => {
     begin: "12:00 am",
     end: "3:00 pm",
     branchId: "2",
+    employeeId: "1"
+  });
+  ServiceShift.create({
+    date: "20/09",
+    begin: "12:00 am",
+    end: "3:00 pm",
+    branchId: "3",
+    employeeId: "1"
+  });
+  ServiceShift.create({
+    date: "21/09",
+    begin: "10:00 am",
+    end: "4:00 pm",
+    branchId: "4",
     employeeId: "1"
   });
   ServiceShift.create({
