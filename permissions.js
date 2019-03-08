d924b54ff5d88add24a4dd0c3e414cce065b62e1
bigeyestoad <rcnavarrop@gmail.com>
2019-03-08 15:56:44 -0500

    progress adding user field to token decoded

--- permissions.js
+++ permissions.js
@@ -2,10 +2,6 @@ const createResolver = (resolver) => {
   const baseResolver = resolver;
     baseResolver.createResolver = (childResolver) => {
       const newResolver = async (parent, args, context, info) => {
-        // console.log("parent", parent);
-        // console.log("args", args);
-        // console.log("context", context);
-        // console.log("info", info);
         await resolver(parent, args, context, info);
         return childResolver(parent, args, context, info);
       };
@@ -15,9 +11,6 @@ const createResolver = (resolver) => {
   };
   
   export default createResolver((parent, args, context) => {
-    // console.log("context.user", context.user === true );
-    // console.log("context.id", context.id );
-    // console.log("context", context );
     if (!context.user || !context.user.id) {
       throw new Error('Not authenticated');
     }
